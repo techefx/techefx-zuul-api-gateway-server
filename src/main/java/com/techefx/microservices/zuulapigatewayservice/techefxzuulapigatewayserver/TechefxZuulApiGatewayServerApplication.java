ebe578020428788f9e6ebd88e49d9f7eb9d4f071
techefx <techefx2@gmail.com>
2020-12-02 02:21:36 +0530

    Uploaded Zuul API Gateway Proxy
    Uploaded Zuul API Gateway Proxy

---
+++ src/main/java/com/techefx/microservices/zuulapigatewayservice/techefxzuulapigatewayserver/TechefxZuulApiGatewayServerApplication.java
@@ -0,0 +1,15 @@
+package com.techefx.microservices.zuulapigatewayservice.techefxzuulapigatewayserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableZuulProxy
+public class TechefxZuulApiGatewayServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TechefxZuulApiGatewayServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/techefx/microservices/zuulapigatewayservice/techefxzuulapigatewayserver/TechefxZuulApiGatewayServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techefx.microservices.zuulapigatewayservice.techefxzuulapigatewayserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechefxZuulApiGatewayServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
